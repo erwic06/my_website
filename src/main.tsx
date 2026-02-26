ea7fe49b260f8050498386d69bac4ef8be0c2c89
Eric Wong <sugoeric15@gmail.com>
2026-02-26 00:06:29 -0600

    vercel deployment and vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
